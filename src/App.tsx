148c99fc0e9e6b19bbfe0663087547a0871e302c
sonivk10 <varun.soni9137@softsensor.ai>
2024-06-20 14:43:48 +0530

    test2commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo from './Logo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React the hard way
         </a>
       </header>
     </div>
