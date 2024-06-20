df82ac62af8b43516b8326483a4b6e3af5911701
yash raj <yash.raj9132@softsensor.ai>
2024-06-20 14:57:31 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React the hard way
+          Learn React the hard way. hello there. 
         </a>
       </header>
     </div>
